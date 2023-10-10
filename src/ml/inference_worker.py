1978005bb81ac2a2cd8be06a8d910d30b280a0ba
Tony Yu <itsfishpain@gmail.com>
2023-10-10 18:38:28 +0800

    extracted preprocessor into its own class

--- src/ml/inference_worker.py
+++ src/ml/inference_worker.py
@@ -1,72 +1,30 @@
 import joblib
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from lime import lime_text
 from typing import List, Tuple, Dict
 from constants import ModelsConst
-from gensim.parsing.porter import PorterStemmer
 from src.ml.download_helper import download_asset
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from src.ml.preprocess_worker import Preprocessor
 
-nltk.download("punkt")
-
-
-class InferenceWorker:
-    def __init__(self, data: Dict) -> pd.DataFrame:
+class InferenceWorker(Preprocessor):
+    def __init__(self, data: Dict) -> None:
         self.is_model_downloaded = download_asset()
-        """ preprocess data and return dataframe with features"""
+
         df = pd.DataFrame(data, index=[0])
-
-        # Columns to check and add if missing
         columns_to_check = ["description", "requirements", "benefits"]
-
-        # Check if columns are missing
         missing_columns = [col for col in columns_to_check if col not in df.columns]
-
-        # Add missing columns and fill with np.nan
         for col in missing_columns:
             df[col] = np.nan
 
         df = df[["description", "requirements", "benefits"]].fillna("")
-        df["feature"] = (
-            df["description"] + " " + df["requirements"] + " " + df["benefits"]
-        )
-        df = df[["feature"]]
-        df["feature"] = df["feature"].str.lower()
-        # remove html tags and word that start with & and \
-        df["feature"] = df["feature"].str.replace(r"<[^>]*>", "")
-        df["feature"] = df["feature"].str.replace(r"&[^;]*;", "")
-        df["feature"] = df["feature"].str.replace(r"\\[a-z]*", "")
-        # remove punctuation
-        df["feature"] = df["feature"].str.replace(r"[^\w\s]", "")
-        # remove digits
-        df["feature"] = df["feature"].str.replace(r"\d+", "")
-        # remove whitespace
-        df["feature"] = df["feature"].str.replace(r"\s+", " ")
-        # tokenize
-        df["feature"] = df["feature"].apply(lambda x: word_tokenize(x.lower()))
-        # remove stopwords
-        all_stopwords = set(stopwords.words("english"))
-        all_stopwords.update(["\\r\\n"])
-        df["feature"] = df["feature"].apply(
-            lambda x: [word for word in x if word not in all_stopwords]
-        )
-        # stem words
-        df["feature"] = df["feature"].apply(
-            lambda x: [PorterStemmer().stem(word) for word in x]
-        )
-        df["feature"] = df["feature"].apply(
-            lambda x: [word for word in x if len(word) >= 3]
-        )
-        df["feature"] = df["feature"].apply(lambda x: " ".join(x))
+        super().__init__(df, type="inference")
         self.x_text = df["feature"]
 
     def predict(self, model: str = ModelsConst.RANDOM_FOREST.value) -> List[int]:
         if self.is_model_downloaded == False:
-            raise Exception("Model not downloaded")
+            raise Exception(f"Model not downloaded, please download it manually from \
+                            https://github.com/FishPain/sure-bo/releases/download/v0.1.0/{model}.pkl")
 
         self.model = joblib.load(f"src/models/{model}.pkl")
         self.pred = self.model.predict(self.x_text)
